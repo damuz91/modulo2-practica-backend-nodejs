f0a732e26a782178b39307f88d62045601f2b963
David M <damuz91@gmail.com>
2022-08-21 17:54:01 -0500

    Agregado middleware de params

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
    res.send('Esta es una sumadora. Envía por POST los 2 parámetros a sumar');
@@ -7,10 +9,10 @@ app.get('/', function (req, res) {
 
 app.post('/', function(req, res) {
    console.log("Recibí una solicitud de suma")
-   console.log("Los parámetros son: " + req.params)
-   console.log("Los parámetros son: " + req.params.num1 + " y " + req.params.num2)
-   var a = parseInt(req.params.num1);
-   var b = parseInt(req.params.num2);
+   console.log("Los parámetros son: " + req.body)
+   console.log("Los parámetros son: " + req.body.num1 + " y " + req.body.num2)
+   var a = parseInt(req.body.num1);
+   var b = parseInt(req.body.num2);
    var c = a + b;
    res.send(c.toString());
 });
